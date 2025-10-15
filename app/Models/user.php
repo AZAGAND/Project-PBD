adbcad43c10d6f559ebb29636bc999a52932fa1d
ZaganJade <playerthebest042@gmail.com>
2025-10-15 16:10:55 +0700

    fix test/ci push gothub

--- app/Models/user.php
+++ app/Models/user.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class user extends Authenticatable
 {
     use Notifiable;
 
