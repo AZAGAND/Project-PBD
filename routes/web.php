3cd8c20b3e894aab0d637a1346ca1d5aa5e7ed84
ZaganJade <playerthebest042@gmail.com>
2025-11-13 14:16:59 +0700

    fix middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Auth;
@@ -20,17 +19,8 @@ Route::get('/', function() {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return redirect()->route('dashboard');
-})->name('home');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-
 Route::get('/login', LoginForm::class)->name('login')->middleware('guest');
+
 Route::post('/logout', function () {
     Auth::logout();
     request()->session()->invalidate();
@@ -40,26 +30,34 @@ Route::post('/logout', function () {
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
 
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // SETTINGS
+    Route::redirect('settings', 'settings/profile');
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+
+    // MASTER
+    Route::prefix('master')->group(function () {
+        Route::get('role',   RoleCrud::class)->name('master.role');
+        Route::get('user',   UserCrud::class)->name('master.user');
+        Route::get('vendor', VendorCrud::class)->name('master.vendor');
+        Route::get('satuan', SatuanCrud::class)->name('master.satuan');
+        Route::get('barang', BarangCrud::class)->name('master.barang');
+        Route::get('MarginPenjualan', MarginPenjualan::class)->name('master.marginPenjualan');
+    });
+
+    // TRANSACTION
+    Route::prefix('transaction')->group(function () {
+        Route::get('pengadaan', Pengadaan::class)->name('transaction.pengadaan');
+        Route::get('pengadaan/{id}/detail', DetailPengadaan::class)->name('transaction.pengadaan.detail');
+        Route::get('Penerimaan', Penerimaan::class)->name('transaction.Penerimaan');
+    });
+
 });
 
 require __DIR__.'/auth.php';
-
-Route::prefix('master')->group(function () {
-    Route::get('role',   RoleCrud::class)->name('master.role');
-    Route::get('user',   UserCrud::class)->name('master.user');
-    Route::get('vendor', VendorCrud::class)->name('master.vendor');
-    Route::get('satuan', SatuanCrud::class)->name('master.satuan');
-    Route::get('barang', BarangCrud::class)->name('master.barang');
-    route::get('MarginPenjualan', MarginPenjualan::class)->name('master.marginPenjualan');
-});
-
-route::prefix('transaction')->group(function(){
-    route::get('pengadaan', Pengadaan::class)->name('transaction.pengadaan');
-    Route::get('pengadaan/{id}/detail', DetailPengadaan::class)->name('transaction.pengadaan.detail');
-    route::get('Penerimaan', Penerimaan::class)->name('transaction.Penerimaan');
-});
